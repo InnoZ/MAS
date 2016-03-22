8fb59a0b1dd54e3e17834e8361da01195db9e731
dhosse <dhosse08@gmail.com>
2016-03-22 13:36:13 +0100

    some comments

--- code/InnozToolbox/src/innoz/db/MobilityDatabaseMain.java
+++ code/InnozToolbox/src/innoz/db/MobilityDatabaseMain.java
@@ -3,8 +3,19 @@ package innoz.db;
 import playground.dhosse.scenarios.generic.Configuration;
 import playground.dhosse.scenarios.generic.population.io.mid.MiDParser;
 
+/**
+ * 
+ * Test class for database connection and population generation.
+ * 
+ * @author dhosse
+ *
+ */
 public class MobilityDatabaseMain {
 
+	/**
+	 * 
+	 * @param args configuration file
+	 */
 	public static void main(String args[]){
 		
 		if(args.length > 0){
