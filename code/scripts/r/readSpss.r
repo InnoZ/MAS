3c35174ff0390c839a30e0c34e17bd2b2de294c4
dhosse <dhosse08@gmail.com>
2016-03-23 16:56:20 +0100

    minor adjustments in r code

--- code/scripts/r/readSpss.r
+++ code/scripts/r/readSpss.r
@@ -5,13 +5,17 @@ library(foreign)
 #read spss file
 #use.value.labels = if labels that are assigned to numeric values should be displayed instead of the values (e.g. 1 for "has driving license" in column "license")
 #to.data.frame = packs the data into a data frame. makes some of the stuff in this script easier
-d <- read.spss("/home/dhosse/Dokumente/10_data/MiD2008/spss/MiD2008_PUF_Wege.sav", use.value.labels=FALSE, to.data.frame=TRUE)
+#re-encoding is needed for non-utf8 characters (e.g. umlaute)
+d <- read.spss("/home/dhosse/Dokumente/10_data/MiD2008/spss/MiD2008_PUF_Wege.sav", use.value.labels=FALSE, to.data.frame=TRUE, reencode="UTF(")
 
+#The part down to write.data is not used for the actual process
+###############################################################
 #filter by bundesland and raumtyp (or sth else)
 filter <- (d$sgtyp == 40 )&( d$bland == 9)
 
 #filtering of the data
 data <- d[filter,]
+###############################################################
 
 #write the filtered data into a table (text) file. sep stands for the separating character
 write.table(data, "/home/dhosse/Dokumente/10_data/MiD2008/csv/MiD2008_PUF_Wege2.csv", sep=";")
