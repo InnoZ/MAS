d0591a3f555cb98cf526ff2a08c1d219a41fd41f
dhosse <dhosse08@gmail.com>
2016-03-30 17:03:21 +0200

    added a comment

--- code/InnozToolbox/src/innoz/db/MobilityDatabaseMain.java
+++ code/InnozToolbox/src/innoz/db/MobilityDatabaseMain.java
@@ -20,6 +20,9 @@ public class MobilityDatabaseMain {
 		
 		if(args.length > 0){
 
+			//TODO this method actually needs to call playground.dhosse.scenarios.generic.ScenarioBuilder
+			//we will need admin borders to make this call work (otherwise the agents can't be located
+			//in a meaningful way... dh 03/16
 			Configuration configuration = new Configuration(args[0]);
 			
 			MiDParser parser = new MiDParser();
