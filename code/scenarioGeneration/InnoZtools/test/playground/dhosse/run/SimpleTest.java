f6419136d93af656e557bb1d7f13cbc797719323
dhosse <dhosse08@gmail.com>
2016-05-19 15:42:51 +0200

    pseudo test for testing ant build behaviour

---
+++ code/scenarioGeneration/InnoZtools/test/playground/dhosse/run/SimpleTest.java
@@ -0,0 +1,14 @@
+package playground.dhosse.run;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
