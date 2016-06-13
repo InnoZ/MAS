e7482ef87d2db042fc1ee9b90c1d110015057bc8
dhosse <dhosse08@gmail.com>
2016-06-13 16:47:40 +0200

    schema for commuters table

---
+++ code/scripts/sql/commuters/commuters.sql
@@ -0,0 +1,12 @@
+CREATE TABLE commuters(
+from_id character varying,
+from_name character varying,
+to_id character varying,
+to_name character varying,
+amount integer,
+male integer,
+female integer,
+germans integer,
+foreigners integer,
+azubis integer
+);
