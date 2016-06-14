ec814c899735207761d94b78732d88cf9eede352
dhosse <dhosse08@gmail.com>
2016-06-14 11:56:47 +0200

    adapted commuter schema

--- code/scripts/sql/commuters/commuters.sql
+++ code/scripts/sql/commuters/commuters.sql
@@ -1,11 +1,11 @@
 CREATE TABLE commuters(
-from_id character varying,
-from_name character varying,
-to_id character varying,
-to_name character varying,
+home_id character varying,
+home_name character varying,
+work_id character varying,
+work_name character varying,
 amount integer,
-male integer,
-female integer,
+men integer,
+women integer,
 germans integer,
 foreigners integer,
 azubis integer
