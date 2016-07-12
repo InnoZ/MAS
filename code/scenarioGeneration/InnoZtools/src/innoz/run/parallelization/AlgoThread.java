6b045aa1f770f2045d2dc44c90c70b64d4c1f61e
dhosse <dhosse08@gmail.com>
2016-07-12 15:53:18 +0200

    attempt for generic parallelization classes

---
+++ code/scenarioGeneration/InnoZtools/src/innoz/run/parallelization/AlgoThread.java
@@ -0,0 +1,7 @@
+package innoz.run.parallelization;
+
+public abstract class AlgoThread implements Runnable {
+
+	public abstract void init(Object... args);
+
+}
---
+++ code/scenarioGeneration/InnoZtools/src/innoz/run/parallelization/MultithreadedModule.java
@@ -0,0 +1,109 @@
+package innoz.run.parallelization;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.apache.log4j.Logger;
+
+public abstract class MultithreadedModule {
+
+	private final static Logger log = Logger.getLogger(MultithreadedModule.class);
+	
+	int numberOfThreads;
+	Thread[] threads;
+	AlgoThread[] algothreads;
+	int count = 0;
+	
+	private final AtomicReference<Throwable> hadException = new AtomicReference<>(null);
+	private final ExceptionHandler exceptionHandler = new ExceptionHandler(this.hadException);
+	
+	public MultithreadedModule(int numberOfThreads){
+		
+		this.numberOfThreads = numberOfThreads;
+		
+	}
+	
+	public final void initThreads(String className, Object... args){
+		
+		this.hadException.set(null);
+		
+		this.threads = new Thread[this.numberOfThreads];
+		this.algothreads = new AlgoThread[this.numberOfThreads];
+		
+		for(int i = 0; i < this.numberOfThreads; i++){
+			
+			try {
+			
+				AlgoThread algothread = (AlgoThread)Class.forName(className).newInstance();
+				algothread.init(args);
+				Thread thread = new Thread(algothread);
+				thread.setUncaughtExceptionHandler(exceptionHandler);
+				
+				this.threads[i] = thread;
+				this.algothreads[i] = algothread;
+				
+			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	public abstract void handle(Object obj);
+	
+	public final void execute(){
+		
+		for(Thread thread : this.threads){
+			
+			thread.start();
+			
+		}
+		
+		try {
+
+			for(Thread thread : this.threads){
+			
+				thread.join();
+				
+			}
+			
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+		log.info("all " + this.threads.length + " threads finished.");
+		Throwable throwable = this.hadException.get();
+		
+		if (throwable != null) {
+			
+			throw new RuntimeException("Some threads crashed, thus not all objects may have been handled.", throwable);
+			
+		}
+		
+		this.algothreads = null;
+		this.threads = null;
+		this.count = 0;
+		
+	}
+	
+	final static class ExceptionHandler implements UncaughtExceptionHandler {
+
+		private final AtomicReference<Throwable> hadException;
+
+		public ExceptionHandler(final AtomicReference<Throwable> hadException) {
+			this.hadException = hadException;
+		}
+
+		@Override
+		public void uncaughtException(Thread t, Throwable e) {
+			log.error("Thread " + t.getName() + " died with exception. Will stop after all threads finished.", e);
+			this.hadException.set(e);
+		}
+
+	}
+	
+}
