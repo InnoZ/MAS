d919a17d8725ad2fa38d731ef60ca1d82e3c3155
dhosse <dhosse08@gmail.com>
2016-08-02 17:08:09 +0200

    write modified network at the end

--- code/scenarioGeneration/InnoZtools/src/innoz/scenarioGeneration/transit/GTFSToMatsimConverter.java
+++ code/scenarioGeneration/InnoZtools/src/innoz/scenarioGeneration/transit/GTFSToMatsimConverter.java
@@ -10,19 +10,10 @@ import org.matsim.core.network.NetworkWriter;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.pt.transitSchedule.api.TransitScheduleWriter;
 
-import GTFS2PTSchedule.GTFSDefinitions;
-import GTFS2PTSchedule.GTFSDefinitions.RouteTypes;
-
 public class GTFSToMatsimConverter {
 	
 	public static void main(String args[]){
 
-//		System.out.println(RouteTypes.values()[0]);
-//		System.out.println(RouteTypes.values()[1]);
-//		System.out.println(RouteTypes.values()[2]);
-//		System.out.println(RouteTypes.values()[3]);
-//		System.out.println(RouteTypes.values()[4]);
-//		System.out.println(RouteTypes.values()[5]);
 		Scenario scenario = ScenarioUtils.loadScenario(ConfigUtils.createConfig());
 		new MatsimNetworkReader(scenario.getNetwork()).readFile("/home/dhosse/scenarios/3connect/network.xml.gz");
 		new GTFSToMatsimConverter().run(scenario);
@@ -32,7 +23,7 @@ public class GTFSToMatsimConverter {
 	public void run(Scenario scenario){
 		
 		Network network = scenario.getNetwork();
-		String filebase = "/run/user/1000/gvfs/smb-share:server=192.168.0.3,share=gisdata/MODELLINGDATA/3connect/VBNgTF/";
+		String filebase = "/home/dhosse/02_Data/GTFS/VBN/";
 		GTFS2MATSimTransitSchedule g2m = new GTFS2MATSimTransitSchedule(
 				new File[]{new File(filebase)},
 				new String[]{"road","rail"},
