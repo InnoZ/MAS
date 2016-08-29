ec3da7da90fb3d4fcef67533ca10afafd6aa5cc6
dhosse <dhosse08@gmail.com>
2016-08-29 15:34:53 +0200

    comment

--- code/innoz-toolbox/src/main/java/com/innoz/toolbox/utils/data/Tree.java
+++ code/innoz-toolbox/src/main/java/com/innoz/toolbox/utils/data/Tree.java
@@ -60,6 +60,8 @@ public class Tree<T> {
 				
 			} else if(child.data.toString().contains(data.toString())){
 				
+				//TODO There is no guarantee, we get a valid tree, e.g. existing nodes <03444>, <03454> and new node <03>
+				// only one of the existing nodes would be child of the new node
 				swap(data, child);
 				return true;
 				
