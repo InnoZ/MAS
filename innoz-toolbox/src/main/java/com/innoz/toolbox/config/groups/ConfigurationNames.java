c85fd0b411d0e9dc50b84e72416e06f77faf5dcc
dhosse <dhosse08@gmail.com>
2016-11-22 11:30:26 +0100

    constants

---
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/config/groups/ConfigurationNames.java
@@ -0,0 +1,11 @@
+package com.innoz.toolbox.config.groups;
+
+public class ConfigurationNames {
+
+	public static final String GROUP = "group";
+	public static final String NAME = "name";
+	public static final String PARAM = "param";
+	public static final String PARAMETER_SET = "parameterSet";
+	public static final String VALUE = "value";
+	
+}
