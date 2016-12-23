a65aab1d9a68945b61928fd9cbe98637858b1c1a
dhosse <dhosse08@gmail.com>
2016-12-23 09:07:46 +0100

    disabled initial activity start / end times and leg travel times setting

--- innoz-toolbox/src/main/java/com/innoz/toolbox/scenarioGeneration/population/algorithm/SurveyBasedDemandGenerator.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/scenarioGeneration/population/algorithm/SurveyBasedDemandGenerator.java
@@ -643,7 +643,7 @@ public class SurveyBasedDemandGenerator extends DemandGenerationAlgorithm {
 
 	/**
 	 * 
-	 * Creates a MATSim leg from a survey way.
+	 * Creates a MATSim leg from a survey trip.
 	 * 
 	 * @param population The MATSim population
 	 * @param mpe The survey plan element (in this case: way)
@@ -652,15 +652,15 @@ public class SurveyBasedDemandGenerator extends DemandGenerationAlgorithm {
 	private Leg createLeg(Population population,
 			SurveyPlanElement mpe) {
 		
-		SurveyPlanTrip way = (SurveyPlanTrip)mpe;
-		String mode = way.getMainMode();
-		double departure = way.getStartTime();
-		double ttime = way.getEndTime() - departure;
+		SurveyPlanTrip trip = (SurveyPlanTrip)mpe;
+		String mode = trip.getMainMode();
+//		double departure = way.getStartTime();
+//		double ttime = way.getEndTime() - departure;
 		
 		Leg leg = population.getFactory().createLeg(mode);
-		leg.setTravelTime(ttime);
+//		leg.setTravelTime(ttime);
 
-		this.lastLeg = way;
+		this.lastLeg = trip;
 		
 		return leg;
 		
@@ -754,8 +754,9 @@ public class SurveyBasedDemandGenerator extends DemandGenerationAlgorithm {
 		
 		// Create a new activity
 		Activity activity = population.getFactory().createActivityFromCoord(type.split("_")[0], coord);
-		activity.setStartTime(start);
-		activity.setEndTime(end);
+//		activity.setStartTime(start);
+		activity.setMaximumDuration(end - start);
+//		activity.setEndTime(end);
 		if(this.geoinformation.getLanduseType().equals(ActivityLocationsType.FACILITIES)){
 			activity.setFacilityId(((ProxyFacility)this.geoinformation.getLanduseOfType(type)
 					.getClosest(coord.getX(), coord.getY())).get().getId());
@@ -766,7 +767,7 @@ public class SurveyBasedDemandGenerator extends DemandGenerationAlgorithm {
 			// Set the activity duration to at least 1/4 hour if it's been reported shorter to avoid
 			// extremely negative scores
 			activity.setMaximumDuration(900);
-			activity.setEndTime(start + 900);
+//			activity.setEndTime(start + 900);
 			
 		} else{
 				
