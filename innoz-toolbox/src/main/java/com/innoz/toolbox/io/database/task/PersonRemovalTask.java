a5c13b080a8d6cd9c8f53caad3d36026f8d4be32
bmoehring <bmoehring@ThinkPad-Edge-E545>
2017-02-13 15:06:49 +0100

    Log.Info about the number of persons removed from the SurveyDataContainer

--- innoz-toolbox/src/main/java/com/innoz/toolbox/io/database/task/PersonRemovalTask.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/io/database/task/PersonRemovalTask.java
@@ -3,6 +3,10 @@ package com.innoz.toolbox.io.database.task;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.jfree.util.Log;
+
 import com.innoz.toolbox.scenarioGeneration.population.surveys.SurveyDataContainer;
 import com.innoz.toolbox.scenarioGeneration.population.surveys.SurveyPerson;
 
@@ -11,13 +15,19 @@ public class PersonRemovalTask implements DataContainerTask {
 	@Override
 	public void apply(SurveyDataContainer container){
 		
+		Logger.getLogger(org.matsim.matrices.Matrix.class).setLevel(Level.OFF);
+		
 		Set<String> toRemove = new HashSet<>();
+		
+		int personsToBeRemoved = 0;
 		
 		for(SurveyPerson person : container.getPersons().values()){
 			
 			if(person.getLogbook().isEmpty() && person.isMobile()){
 				
 				toRemove.add(person.getId());
+				
+				personsToBeRemoved++;
 				
 			}
 			
@@ -26,6 +36,8 @@ public class PersonRemovalTask implements DataContainerTask {
 		for(String id : toRemove){
 			container.removePerson(id);
 		}
+		
+		Log.info(personsToBeRemoved + " mobile persons without logbook. " + toRemove.size() + " persons removed.");
 		
 	}
 	
