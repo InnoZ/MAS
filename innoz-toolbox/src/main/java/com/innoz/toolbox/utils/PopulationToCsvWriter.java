5fd354b5f3f5e11dcb22301e8abcfd8144783c3f
bmoehring <bmoehring@ThinkPad-Edge-E545>
2017-02-13 15:15:45 +0100

    Class to write the population into shp and csv files

---
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/utils/PopulationToCsvWriter.java
@@ -0,0 +1,104 @@
+package com.innoz.toolbox.utils;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+
+public class PopulationToCsvWriter {
+	
+	/**
+	 * 
+	 * Utilities in terms of geometries.
+	 * 
+	 * @author dhosse
+	 *
+	 */
+
+	//CONSTANTS//////////////////////////////////////////////////////////////////////////////
+	private static final Logger log = Logger.getLogger(GeometryUtils.class);
+	private static final String SPACE = " ";
+	private static final String TRIPLE_SPACE = "   ";
+	private static final String TAB = "\t";
+	private static final String END = "END";
+	/////////////////////////////////////////////////////////////////////////////////////////
+	
+	// No instance!
+	private PopulationToCsvWriter(){};
+	
+	public static void writePopulation2Csv(Population population, String filepath) throws FileNotFoundException{
+		
+		BufferedWriter bw = null;
+		
+		String file = filepath + "/population.csv";
+		int personCount = 0;
+		
+		log.info("reading population");
+		
+		try {
+			
+			bw = new BufferedWriter(new FileWriter(file));
+			
+			String line = "personID;"
+					+ "age;"
+					+ "sex;"
+					+ "license;"
+					+ "car_avail;"
+					+ "employed;"
+					+ "actcount; \n";
+			
+			bw.write(line);
+			
+			
+			for(Person person : population.getPersons().values()){
+				
+				line = null;
+				Map<String, Object> ca = person.getCustomAttributes();
+				
+				line = person.getId().toString() + ";"
+						+ ca.get(com.innoz.toolbox.scenarioGeneration.population.utils.PersonUtils.ATT_SEX).toString() + ";"
+						+ ca.get(com.innoz.toolbox.scenarioGeneration.population.utils.PersonUtils.ATT_AGE).toString() + ";"
+						+ ca.get(com.innoz.toolbox.scenarioGeneration.population.utils.PersonUtils.ATT_LICENSE).toString() + ";"
+						+ ca.get(com.innoz.toolbox.scenarioGeneration.population.utils.PersonUtils.ATT_CAR_AVAIL).toString() + ";"
+						+ String.valueOf(ca.get(com.innoz.toolbox.scenarioGeneration.population.utils.PersonUtils.ATT_EMPLOYED)) + ";"
+						+ person.getSelectedPlan().getPlanElements().size() +  ";\n";
+				
+				if (line != null){
+
+					bw.write(line);
+					personCount ++;
+					
+				}	
+			}
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			log.error("Point feature collection is empty and thus there is no file to write...");
+			
+		} finally {
+			try {
+				
+				if (bw != null){
+					
+					bw.flush();
+					bw.close();
+					
+					log.info(personCount + " persons written to " + file);
+					
+				}
+				
+			} catch ( IOException e) {
+				
+				e.printStackTrace();
+				
+			}
+		}
+	}
+
+}
---
+++ innoz-toolbox/src/test/java/com/innoz/toolbox/scenarioGeneration/population/validation/Population2Shape.java
@@ -0,0 +1,49 @@
+package com.innoz.toolbox.scenarioGeneration.population.validation;
+
+
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import com.innoz.toolbox.utils.GeometryUtils;
+import com.innoz.toolbox.utils.PopulationToCsvWriter;
+
+public class Population2Shape {
+
+	public static void main(String[] args) {
+		
+		final Logger log = Logger.getLogger(Population2Shape.class);
+		
+		log.info("start");
+		
+		try {
+			
+			String input = "/home/bmoehring/scenarios/heidelberg/";
+			String output = input + "validation/";
+			
+			Config config = new Config();
+			
+			ConfigUtils.loadConfig( config, input + "config.xml" );
+			
+			Scenario scenario = ScenarioUtils.loadScenario(config);
+				
+			GeometryUtils.writeActivityLocationsToShapefile(scenario.getPopulation(), output + "facilities", "EPSG:4326");
+//			
+			GeometryUtils.writeNetwork2Shapefile(scenario.getNetwork(), output, "EPSG:4326");
+			
+			PopulationToCsvWriter.writePopulation2Csv(scenario.getPopulation(), output);
+		
+			log.info("done");
+			
+		} catch ( Exception e){
+			
+			e.printStackTrace();
+			
+		}
+
+	}
+
+}
