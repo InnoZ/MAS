2bf61da3f1a009bfc214faa9c6725fb00347995b
bsmoehring <bsmoehring@gmail.com>
2017-02-23 09:58:35 +0100

    Missing Class PersonRegionTypeHandler
     com.innoz.toolbox.io.database.handler.PersonRegionTypeHandler

---
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/io/database/handler/PersonRegionTypeHandler.java
@@ -0,0 +1,28 @@
+package com.innoz.toolbox.io.database.handler;
+
+import java.util.Map;
+
+import com.innoz.toolbox.io.SurveyConstants;
+import com.innoz.toolbox.scenarioGeneration.population.surveys.SurveyObject;
+import com.innoz.toolbox.scenarioGeneration.population.surveys.SurveyPerson;
+
+public class PersonRegionTypeHandler implements DefaultHandler {
+
+	@Override
+	public void handle(SurveyObject obj, Map<String, String> attributes, String surveyType) {
+		
+		SurveyPerson person = (SurveyPerson)obj;
+		
+		String regionType = attributes.get(SurveyConstants.regionType(surveyType));
+		
+		if(regionType != null){
+			
+			person.setRegionType(Integer.parseInt(regionType));
+			
+		}
+		
+	}
+
+	
+
+}
