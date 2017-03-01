f44d39a1cfdb57103eec8288fa2d463f512f9fb9
dhosse <dhosse08@gmail.com>
2017-03-01 09:13:17 +0100

    work on formatting / javadoc

--- innoz-toolbox/src/main/java/com/innoz/toolbox/utils/math/regression/TrendLine.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/utils/math/regression/TrendLine.java
@@ -1,9 +1,18 @@
 package com.innoz.toolbox.utils.math.regression;
 
+/**
+ * 
+ * An interface for different types of trend lines. These trend lines represent the regression of a data set. </br>
+ * 
+ * Source: <a href="http://stackoverflow.com/questions/17592139/trend-lines-regression-curve-fitting-java-library">Stackoverflow<a/>
+ * 
+ * @author dhosse
+ *
+ */
 public interface TrendLine {
 
 	public void setValues(double[] y, double[] x); // y ~ f(x)
     public double predict(double x); // get a predicted y for a given x
-    public double getRSquare();
+    public double getRSquare(); // get the coefficient of determination r^2
 
 }
