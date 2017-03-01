5fa5ecf4aebd90493697cb5bc898caa37af72da2
dhosse <dhosse08@gmail.com>
2017-03-01 09:14:07 +0100

    added validation step for forecast year

--- innoz-toolbox/src/main/java/com/innoz/toolbox/config/validation/ConfigurationValidator.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/config/validation/ConfigurationValidator.java
@@ -17,13 +17,14 @@ public class ConfigurationValidator {
 
 		// Check if the output directory exists and has files in it.
 		File f = new File(configuration.misc().getOutputDirectory());
-		if(f.exists()){
+		
+		if(f.exists()) {
 			
-			if(f.list().length > 0){
+			if(f.list().length > 0) {
 				
 				log.warn("The output directory " + configuration.misc().getOutputDirectory() + " already exists and has files in it!");
 				
-				if(!configuration.misc().isOverwritingExistingFiles()){
+				if(!configuration.misc().isOverwritingExistingFiles()) {
 					
 					log.error("Since you disabled overwriting of existing files, you must either delete existing files or pick another"
 							+ " output directory!");
@@ -41,13 +42,14 @@ public class ConfigurationValidator {
 		
 		int nProcessors = Runtime.getRuntime().availableProcessors();
 		int n = configuration.misc().getNumberOfThreads();
-		if(n > nProcessors){
+		
+		if(n > nProcessors) {
 			
 			log.warn("Specified number of threads: " + n + ", but you have only " + nProcessors + " cores available...");
 			log.info("Thus, the programm will only use these " + nProcessors + " cores.");
 			configuration.misc().setNumberOfThreads(nProcessors);
 			
-		} else if(n == 0){
+		} else if(n == 0) {
 			
 			log.warn("Specified number of threads: " + n + "!");
 			log.info("Thus, the programm will use all " + nProcessors + " cores.");
@@ -55,8 +57,15 @@ public class ConfigurationValidator {
 			
 		}
 		
+		if(configuration.scenario().getYear() > 2040) {
+			
+			log.error("BBSR population forecast data only covers the time until 2040, but you chose " + configuration.scenario().getYear() + "!");
+			validationError = true;
+			
+		}
+		
 		// If anything should cause the configuration to be invalid, abort!
-		if(validationError){
+		if(validationError) {
 			
 			throw new RuntimeException("Invalid configuration! Shutting down...");
 			
