b94c0d1f0372b971efef44becaf291b0f9b2b42f
dhosse <dhosse08@gmail.com>
2017-03-27 13:36:08 +0200

    xml reader now handles highway defaults

--- innoz-toolbox/src/main/java/com/innoz/toolbox/config/ConfigurationUtils.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/config/ConfigurationUtils.java
@@ -1,11 +1,17 @@
 package com.innoz.toolbox.config;
 
+import java.io.InputStream;
+
 public class ConfigurationUtils {
 	
 	private ConfigurationUtils(){};
 
 	public static void loadConfiguration(String file, Configuration configuration){
 		configuration.load(file);
+	}
+	
+	public static void loadConfiguration(InputStream stream, Configuration configuration){
+		configuration.load(stream);
 	}
 	
 	public static Configuration createConfiguration(){
--- innoz-toolbox/src/main/java/com/innoz/toolbox/io/DefaultXmlReader.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/io/DefaultXmlReader.java
@@ -2,20 +2,26 @@ package com.innoz.toolbox.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class DefaultXmlReader extends DefaultHandler {
 
+	private static final Logger log = Logger.getLogger(DefaultXmlReader.class);
+	
 	public void read(String file){
 		
 		File inputFile = new File(file);
 		SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+		
+		log.info("Reading file " + file);
 		
 		try {
 		
@@ -27,6 +33,28 @@ public class DefaultXmlReader extends DefaultHandler {
 			e.printStackTrace();
 			
 		}
+		
+		log.info("Done.");
+		
+	}
+	
+	public void read(InputStream stream) {
+		
+		SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+		
+		try {
+		
+			SAXParser parser = saxFactory.newSAXParser();
+			parser.parse(stream, this);
+		
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+		
+			e.printStackTrace();
+			
+		}
+		
+		log.info("Done.");
+		
 		
 	}
 	
