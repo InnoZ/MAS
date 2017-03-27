daf4a1a0880d177796b412732945b2fe3db9b09a
dhosse <dhosse08@gmail.com>
2017-03-27 13:36:08 +0200

    xml reader now handles highway defaults

--- innoz-toolbox/src/main/java/com/innoz/toolbox/config/ConfigurationUtils.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/config/ConfigurationUtils.java
@@ -1,9 +1,15 @@
 package com.innoz.toolbox.config;
 
+import java.io.InputStream;
+
 public class ConfigurationUtils {
 
 	public static void loadConfiguration(String file, Configuration configuration){
 		configuration.load(file);
+	}
+	
+	public static void loadConfiguration(InputStream stream, Configuration configuration){
+		configuration.load(stream);
 	}
 	
 	public static Configuration createConfiguration(){
