5dff97bdd510336e48743a24ba45925f1e95a346
dhosse <dhosse08@gmail.com>
2017-04-25 16:33:29 +0200

    class that creates a random sample from a given population & os main adaptations for mobility attitude test runs

--- scenarios/src/main/java/com/innoz/scenarios/osnabrück/OsMain.java
+++ scenarios/src/main/java/com/innoz/scenarios/osnabrück/OsMain.java
@@ -1,126 +1,262 @@
 package com.innoz.scenarios.osnabrück;
 
-import java.util.Arrays;
-
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
-import org.matsim.contrib.carsharing.config.CarsharingConfigGroup;
-import org.matsim.contrib.carsharing.config.FreeFloatingConfigGroup;
-import org.matsim.contrib.carsharing.config.OneWayCarsharingConfigGroup;
-import org.matsim.contrib.carsharing.config.TwoWayCarsharingConfigGroup;
-import org.matsim.contrib.carsharing.runExample.RunCarsharing;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
-import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ModeParams;
-import org.matsim.core.config.groups.StrategyConfigGroup;
 import org.matsim.core.config.groups.StrategyConfigGroup.StrategySettings;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule.DefaultSelector;
 import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule.DefaultStrategy;
 import org.matsim.core.scenario.ScenarioUtils;
 
+import com.innoz.toolbox.matsim.scoring.MobilityAttitudeConfigGroup;
+import com.innoz.toolbox.matsim.scoring.MobilityAttitudeConfigGroup.MobilityAttitudeModeParameterSet;
+import com.innoz.toolbox.matsim.scoring.MobilityAttitudeConfigGroup.MobilityAttitudeModeParams;
+import com.innoz.toolbox.matsim.scoring.MobilityAttitudeScoringFunctionFactory;
+
 public class OsMain {
 
+	static String filebase = "/home/dhosse/scenarios/mobilityAttitude/osnabrueck/";
+	
 	public static void main(String args[]){
 		
-		Config config = ConfigUtils.loadConfig("/home/dhosse/scenarios/3connect/config.xml.gz",
-				new CarsharingConfigGroup(), new OneWayCarsharingConfigGroup(), new TwoWayCarsharingConfigGroup(), new FreeFloatingConfigGroup());
-		
-		config.plansCalcRoute().setInsertingAccessEgressWalk(false);
-		
-		config.vspExperimental().setWritingOutputEvents(true);
+		Config config = ConfigUtils.loadConfig(filebase + "config.xml.gz");
+
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 		
-		config.qsim().setMainModes(Arrays.asList(TransportMode.car, "freefloating_vehicle",
-				"twoway_vehicle", "oneway_vehicle"));
-		
-		StrategyConfigGroup strategy = config.strategy();
-		{
-			StrategySettings stratSets = new StrategySettings();
-			stratSets.setStrategyName(DefaultSelector.ChangeExpBeta);
-			stratSets.setWeight(0.7);
-			strategy.addStrategySettings(stratSets);
-		}
-		{
-			StrategySettings stratSets = new StrategySettings();
-			stratSets.setStrategyName(DefaultStrategy.ReRoute.name());
-			stratSets.setWeight(0.2);
-			strategy.addStrategySettings(stratSets);
-		}
-		{
-			StrategySettings stratSets = new StrategySettings();
-			stratSets.setStrategyName(DefaultStrategy.ChangeTripMode.name());
-			stratSets.setWeight(0.1);
-			strategy.addStrategySettings(stratSets);
-		}
-		{
-			StrategySettings stratSets = new StrategySettings();
-			stratSets.setStrategyName("CarsharingSubtourModeChoiceStrategy");
-			stratSets.setWeight(0.05);
-			strategy.addStrategySettings(stratSets);
-		}
-		{
-			StrategySettings stratSets = new StrategySettings();
-			stratSets.setStrategyName("RandomTripToCarsharingStrategy");
-			stratSets.setWeight(0.05);
-			strategy.addStrategySettings(stratSets);
-		}
-		
-		PlanCalcScoreConfigGroup planCalcScore = config.planCalcScore();
-		{
-			ModeParams params = new ModeParams(TransportMode.ride);
-			params.setConstant(0d);
-			params.setMarginalUtilityOfTraveling(0d);
-			planCalcScore.addModeParams(params);
-		}
-		{
-			ModeParams params = new ModeParams("access_walk_tw");
-			params.setConstant(0d);
-			params.setMarginalUtilityOfTraveling(0d);
-			planCalcScore.addModeParams(params);
-		}
-		{
-			ModeParams params = new ModeParams("access_walk_ff");
-			params.setConstant(0d);
-			params.setMarginalUtilityOfTraveling(0d);
-			planCalcScore.addModeParams(params);
-		}
-		{
-			ModeParams params = new ModeParams("egress_walk_tw");
-			params.setConstant(0d);
-			params.setMarginalUtilityOfTraveling(0d);
-			planCalcScore.addModeParams(params);
-		}
-		{
-			ModeParams params = new ModeParams("egress_walk_ff");
-			params.setConstant(0d);
-			params.setMarginalUtilityOfTraveling(0d);
-			planCalcScore.addModeParams(params);
-		}
-		{
-			ModeParams params = new ModeParams("twoway_vehicle");
-			params.setConstant(0d);
-			params.setMarginalUtilityOfTraveling(0d);
-			planCalcScore.addModeParams(params);
-		}
-		{
-			ModeParams params = new ModeParams("freefloating_vehicle");
-			params.setConstant(0d);
-			params.setMarginalUtilityOfTraveling(0d);
-			planCalcScore.addModeParams(params);
-		}
+		addStrategySettings(config);
+		addMobilityAttitudeConfigGroup(config);
 		
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 		
-		config.plansCalcRoute().setInsertingAccessEgressWalk(true);
-
 		Controler controler = new Controler(scenario);
 		
-		RunCarsharing.installCarSharing(controler);
-		
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				bindScoringFunctionFactory().to(MobilityAttitudeScoringFunctionFactory.class);
+			}
+		});
+
 		controler.run();
+		
+	}
+	
+	private static void addStrategySettings(Config config) {
+		
+		{
+			StrategySettings stratSets = new StrategySettings();
+			stratSets.setDisableAfter(-1);
+			stratSets.setStrategyName(DefaultSelector.ChangeExpBeta.name());
+			stratSets.setSubpopulation(null);
+			stratSets.setWeight(0.7);
+			config.strategy().addStrategySettings(stratSets);
+		}
+		
+		{
+			StrategySettings stratSets = new StrategySettings();
+			stratSets.setDisableAfter(-1);
+			stratSets.setStrategyName(DefaultStrategy.ReRoute.name());
+			stratSets.setSubpopulation(null);
+			stratSets.setWeight(0.2);
+			config.strategy().addStrategySettings(stratSets);
+		}
+		
+		{
+			StrategySettings stratSets = new StrategySettings();
+			stratSets.setDisableAfter(-1);
+			stratSets.setStrategyName(DefaultStrategy.SubtourModeChoice.name());
+			stratSets.setSubpopulation(null);
+			stratSets.setWeight(0.1);
+			config.strategy().addStrategySettings(stratSets);
+		}
+		
+	}
+	
+	private static void addMobilityAttitudeConfigGroup(Config config) {
+		
+		MobilityAttitudeConfigGroup ma = new MobilityAttitudeConfigGroup();
+		ma.setSubpopulationAttribute("mobilityAttitude");
+		ma.setScaleFactor(10d);
+		
+		{
+			MobilityAttitudeModeParameterSet pars = new MobilityAttitudeModeParameterSet();
+			pars.setAttitudeGroup("tradCar");
+			
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.car);
+				params.setOffset(0.5509808129);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.bike);
+				params.setOffset(-0.6728124661);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.pt);
+				params.setOffset(-1.3728579165);
+				pars.addModeParams(params);
+			}
+			
+			
+			ma.addModeParams(pars);
+		}
+		
+		{
+			MobilityAttitudeModeParameterSet pars = new MobilityAttitudeModeParameterSet();
+			pars.setAttitudeGroup("flexCar");
+			
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.car);
+				params.setOffset(0.6380481272);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.bike);
+				params.setOffset(0.1034697375);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.pt);
+				params.setOffset(-0.2605919306);
+				pars.addModeParams(params);
+			}
+			
+			
+			ma.addModeParams(pars);
+		}
+		
+		{
+			MobilityAttitudeModeParameterSet pars = new MobilityAttitudeModeParameterSet();
+			pars.setAttitudeGroup("urbanPt");
+			
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.car);
+				params.setOffset(-0.7808252647);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.bike);
+				params.setOffset(-1.6880941473);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.pt);
+				params.setOffset(0.700257666);
+				pars.addModeParams(params);
+			}
+			
+			
+			ma.addModeParams(pars);
+		}
+		
+		{
+			MobilityAttitudeModeParameterSet pars = new MobilityAttitudeModeParameterSet();
+			pars.setAttitudeGroup("convBike");
+			
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.car);
+				params.setOffset(-0.582954545);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.bike);
+				params.setOffset(0.5985309311);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.pt);
+				params.setOffset(0.3420721502);
+				pars.addModeParams(params);
+			}
+			
+			
+			ma.addModeParams(pars);
+		}
+		
+		{
+			MobilityAttitudeModeParameterSet pars = new MobilityAttitudeModeParameterSet();
+			pars.setAttitudeGroup("envtPtBike");
+			
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.car);
+				params.setOffset(-0.5953667551);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.bike);
+				params.setOffset(0.4392845925);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.pt);
+				params.setOffset(0.5825576487);
+				pars.addModeParams(params);
+			}
+			
+			
+			ma.addModeParams(pars);
+		}
+		
+		{
+			MobilityAttitudeModeParameterSet pars = new MobilityAttitudeModeParameterSet();
+			pars.setAttitudeGroup("multiOpt");
+			
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.car);
+				params.setOffset(0.3446035305);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.bike);
+				params.setOffset(0.3974286492);
+				pars.addModeParams(params);
+			}
+			{
+				MobilityAttitudeModeParams params = new MobilityAttitudeModeParams();
+				params.setMode(TransportMode.pt);
+				params.setOffset(0.2730661675);
+				pars.addModeParams(params);
+			}
+			
+			
+			ma.addModeParams(pars);
+		}
+		
+		{
+			MobilityAttitudeModeParameterSet pars = new MobilityAttitudeModeParameterSet();
+			pars.setAttitudeGroup(null);
+			ma.addModeParams(pars);
+		}
+		
+		{
+			MobilityAttitudeModeParameterSet pars = new MobilityAttitudeModeParameterSet();
+			pars.setAttitudeGroup("none");
+			ma.addModeParams(pars);
+		}
+		
+		config.addModule(ma);
 		
 	}
 	
