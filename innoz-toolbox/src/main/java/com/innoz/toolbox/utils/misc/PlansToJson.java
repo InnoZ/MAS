4a3d717b7450b8f56e7fd707e3230503c4fcb34a
dhosse <dhosse08@gmail.com>
2017-05-09 15:59:17 +0200

    hide constructor, added person id field

--- innoz-toolbox/src/main/java/com/innoz/toolbox/utils/misc/PlansToJson.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/utils/misc/PlansToJson.java
@@ -3,7 +3,6 @@ package com.innoz.toolbox.utils.misc;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.geotools.feature.DefaultFeatureCollection;
 import org.geotools.geojson.feature.FeatureJSON;
@@ -13,10 +12,6 @@ import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
-import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.gbl.MatsimRandom;
-import org.matsim.core.population.MatsimPopulationReader;
-import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.core.utils.geometry.geotools.MGC;
@@ -39,10 +34,14 @@ import com.vividsolutions.jts.geom.Coordinate;
  */
 public class PlansToJson {
 	
+	static final String PAR_ID = "person_id";
 	static final String PAR_DEPARTURE = "started_at";
 	static final String PAR_ARRIVAL = "finished_at";
 	static final String PAR_MODE = "mode";
 	static final String PAR_SPEED = "kmh";
+	
+	// non-instantiable
+	private PlansToJson(){};
 	
 	public static void run(final Scenario scenario, String outputFile, String crs) {
 		
@@ -78,6 +77,7 @@ public class PlansToJson {
 					double endTime = leg.getDepartureTime() != Time.UNDEFINED_TIME ? leg.getDepartureTime() + leg.getTravelTime() : toAct.getStartTime();
 					
 					SimpleFeature feature = pfactory.createPolyline(new Coordinate[]{from,to});
+					feature.setAttribute(PAR_ID, person.getId().toString());
 					feature.setAttribute(PAR_DEPARTURE, startTime);
 					feature.setAttribute(PAR_ARRIVAL, endTime);
 					feature.setAttribute(PAR_MODE, leg.getMode());
