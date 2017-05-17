114265e2f3730fa503a9e6883a798184d2855a4f
bmoehring <bmoehring@ThinkPad-Edge-E545>
2017-05-17 11:59:04 +0200

    write person attributes

--- innoz-toolbox/src/main/java/com/innoz/toolbox/run/controller/task/WriteOutputTask.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/run/controller/task/WriteOutputTask.java
@@ -4,6 +4,7 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.ConfigWriter;
 import org.matsim.core.network.NetworkWriter;
 import org.matsim.core.population.PopulationWriter;
+import org.matsim.utils.objectattributes.ObjectAttributesXmlWriter;
 
 import com.innoz.toolbox.config.Configuration;
 import com.innoz.toolbox.utils.misc.PlansToJson;
@@ -28,6 +29,7 @@ public class WriteOutputTask implements ControllerTask {
 		new ConfigWriter(scenario.getConfig()).write(this.outputDirectory + "config.xml.gz");
 		new NetworkWriter(this.scenario.getNetwork()).write(this.outputDirectory + "network.xml.gz");
 		new PopulationWriter(scenario.getPopulation()).write(this.outputDirectory + "plans.xml.gz");
+		new ObjectAttributesXmlWriter(scenario.getPopulation().getPersonAttributes()).writeFile(this.outputDirectory + "personAttributes.xml.gz");
 		PlansToJson.run(this.scenario, this.outputDirectory + "features.json", this.crs);
 
 	}
