bf1ea0012861d52a151a626624cfeb4edfc6469b
bmoehring <bmoehring@ThinkPad-Edge-E545>
2017-06-14 12:44:26 +0200

    Write personAttributes as output

--- innoz-toolbox/src/main/java/com/innoz/toolbox/run/controller/task/WriteOutputTask.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/run/controller/task/WriteOutputTask.java
@@ -3,6 +3,7 @@ package com.innoz.toolbox.run.controller.task;
 import org.matsim.api.core.v01.network.NetworkWriter;
 import org.matsim.api.core.v01.population.PopulationWriter;
 import org.matsim.core.config.ConfigWriter;
+import org.matsim.utils.objectattributes.ObjectAttributesXmlWriter;
 
 import com.innoz.toolbox.io.pgsql.MatsimPsqlAdapter;
 import com.innoz.toolbox.run.controller.Controller;
@@ -26,6 +27,15 @@ public class WriteOutputTask implements ControllerTask {
 		MatsimPsqlAdapter.writeScenarioToPsql(Controller.scenario(), scenarioName, railsEnvironment);
 		new NetworkWriter(Controller.scenario().getNetwork()).write(Controller.configuration().misc().getOutputDirectory() + "network.xml.gz");
 		new PopulationWriter(Controller.scenario().getPopulation()).write(Controller.configuration().misc().getOutputDirectory() + "plans.xml.gz");
+//		MatsimPsqlAdapter.writeScenarioToPsql(Controller.scenario(), scenarioName);
+		new ConfigWriter(Controller.scenario().getConfig()).write(
+				Controller.configuration().misc().getOutputDirectory() + "config.xml.gz");
+		new NetworkWriter(Controller.scenario().getNetwork()).write(
+				Controller.configuration().misc().getOutputDirectory() + "network.xml.gz");
+		new PopulationWriter(Controller.scenario().getPopulation()).write(
+				Controller.configuration().misc().getOutputDirectory() + "plans.xml.gz");
+		new ObjectAttributesXmlWriter(Controller.scenario().getPopulation().getPersonAttributes()).writeFile(
+				Controller.configuration().misc().getOutputDirectory() + "personAttributes.xml.gz");
 //		MatsimPsqlAdapter.writeScenarioToPsql(Controller.scenario(), scenarioName);
 
 	}
