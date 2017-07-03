73da1f59592dd78c4cb13612e99b5d54d708711f
bmoehring <bmoehring@ThinkPad-Edge-E545>
2017-07-03 11:22:23 +0200

    looping MATSim runs to adjust mode parameters
    entrySet is defined by Map modalSplitGoal.
    Constant adjusted after each run. Delta defines the difference absolute
    difference between the aimed and current leg shares.
    
    Note: the adjustment works by listening to the leg histogram, not modal share

--- innoz-toolbox/src/main/java/com/innoz/toolbox/run/callibration/ASCModalSplitCallibration.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/run/callibration/ASCModalSplitCallibration.java
@@ -1,9 +1,11 @@
 package com.innoz.toolbox.run.callibration;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.core.config.Config;
 
 public class ASCModalSplitCallibration {
 	
@@ -16,33 +18,63 @@ public class ASCModalSplitCallibration {
 		
 	}
 	
-	public double calculateModeConstant(String mode, Double cModeOld){
+	public HashMap<String, Double> calculateModeConstants(Config config){
 		
-		double cModeNew;;
+		HashMap<String, Double> constants = new HashMap<String, Double>();
 		
-//		try {
-			cModeNew = cModeOld - Math.log(
-					ASCModalSplitCallibration.modalSplitLatest.get(mode) 
-					/ ASCModalSplitCallibration.modalSplitGoal.get(mode));
-//		} catch (Exception e){
-//			cModeNew = cModeOld;
-//		}
+		for (String mode : ASCModalSplitCallibration.getModalSplitGoal().keySet()){
+			
+			double cModeOld = config.planCalcScore().getOrCreateModeParams(mode).getConstant();
+			double cModeNew;
+			
+			if(ASCModalSplitCallibration.modalSplitGoal.get(mode)==0.0){
+				cModeNew = 0.0;
+			} else {
+			
+				try {	
+					cModeNew = cModeOld - Math.log(
+							ASCModalSplitCallibration.modalSplitLatest.get(mode) 
+							/ ASCModalSplitCallibration.modalSplitGoal.get(mode));
+				} catch (Exception e){
+					cModeNew = cModeOld;
+				}
+			}
+			constants.put(mode, cModeNew);
+		}
 		
-		return cModeNew;
+		return constants;
 	}
 	
 	public double calculateDelta(){
 		double delta = 0;
 		
-		for(Entry<String, Double> e : modalSplitGoal.entrySet()){
-			delta += Math.abs(e.getValue() - ASCModalSplitCallibration.modalSplitLatest.get(e.getKey()));
+		for(Entry<String, Double> e : ASCModalSplitCallibration.modalSplitGoal.entrySet()){
+			
+			double d;
+			try {
+				
+				d = Math.abs(e.getValue() - ASCModalSplitCallibration.modalSplitLatest.get(e.getKey()));
+		
+			} catch (NullPointerException exception) {
+				
+				d = Math.abs(e.getValue());
+				
+			}
+			delta += d;
+			
+			System.out.println(delta + " " + e.getKey() + " " + e.getValue());
+			
+			
 		}
 		
 		return delta;
 	}
 	
 	public static void updateModalSplit(Map<String, Double> modalSplit){
+		
 		ASCModalSplitCallibration.modalSplitLatest = modalSplit;
+		System.out.println(ASCModalSplitCallibration.modalSplitLatest);
+		
 	}
 
 	public static Map<String, Double> getModalSplitGoal() {
