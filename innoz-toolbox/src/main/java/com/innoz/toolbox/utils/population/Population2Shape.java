0b3f7b1518250abb9ed364840aad1ce2da35a99d
bmoehring <bmoehring@ThinkPad-Edge-E545>
2017-07-10 12:02:39 +0200

    set mode to hold c=0, adapt network changes from shapefile

--- innoz-toolbox/src/main/java/com/innoz/toolbox/utils/population/Population2Shape.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/utils/population/Population2Shape.java
@@ -2,8 +2,14 @@ package com.innoz.toolbox.utils.population;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.NetworkWriter;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.scenario.ScenarioUtils;
@@ -22,20 +28,32 @@ public class Population2Shape {
 		
 		try {
 			
-			String input = "/home/bmoehring/scenarios/heidelberg/";
-			String output = input + "validation/";
+			String input = "/home/bmoehring/scenarios/osnabrueck/03404_2017/";
+			String output = "/home/bmoehring/3connect/Scenarios/11: Fahrverbot Verbrenner/";
 			
 			Config config = new Config();
 			
-			ConfigUtils.loadConfig( config, input + "config.xml" );
+			ConfigUtils.loadConfig( config, input + "config.xml.gz" );
 			
 			Scenario scenario = ScenarioUtils.loadScenario(config);
 				
-			GeometryUtils.writeActivityLocationsToShapefile(scenario.getPopulation(), output + "facilities", "EPSG:4326");
-//			
-			GeometryUtils.writeNetwork2Shapefile(scenario.getNetwork(), output, "EPSG:4326");
+//			GeometryUtils.writeActivityLocationsToShapefile(scenario.getPopulation(), output + "facilities", "EPSG:4326");
 			
-			PopulationToCsvWriter.writePopulation2Csv(scenario.getPopulation(), output);
+			GeometryUtils.writeNetwork2Shapefile(scenario.getNetwork(), output , "EPSG:32326"); //"EPSG:4326"
+			
+//			List<String> icLinks = new ArrayList<String>();
+//			icLinks.add("35454");
+
+//			Network network = scenario.getNetwork();
+//			for(Link l : network.getLinks().values()){
+//				if (icLinks.contains(l.getId().toString())){
+//					l.getAttributes().putAttribute("innercity", "yes");
+//				} else {
+//					l.getAttributes().putAttribute("innercity", "no");
+//				}
+//			}
+			
+//			new NetworkWriter(network).write(output + "mergedNetworkInnerCity.xml.gz");
 		
 			log.info("done");
 			
