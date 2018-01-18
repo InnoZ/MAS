099b01483ace150b31e5e97426681d863804692a
bmoehring <bmoehring@ThinkPad-Edge-E545>
2018-01-18 10:12:34 +0100

    access attribute, link coordinates
    when using an alternative nearest link due to restricted access link
    coords are now written to facilities. this allows the simulation to
    determine access-egress_walks combined with the alternative routing

--- innoz-toolbox/src/main/java/com/innoz/toolbox/matsim/examples/AccessRestrictionExample.java
+++ innoz-toolbox/src/main/java/com/innoz/toolbox/matsim/examples/AccessRestrictionExample.java
@@ -10,6 +10,7 @@ import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.NetworkFactory;
+import org.matsim.api.core.v01.network.NetworkWriter;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Leg;
@@ -37,7 +38,7 @@ public class AccessRestrictionExample {
 		Config config = ConfigUtils.createConfig();
 		
 		config.controler().setLastIteration(0);
-		config.controler().setOutputDirectory("/home/dhosse/scenarios/accessTest/");
+		config.controler().setOutputDirectory("/home/bmoehring/3connect/TestNetworkRoutingWithAccess/output_test/");
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 		
 		ActivityParams params = new ActivityParams();
@@ -51,7 +52,7 @@ public class AccessRestrictionExample {
 		AccessConfigGroup acg = new AccessConfigGroup();
 		acg.setAccessAttribute("innercity");
 		acg.setMode(TransportMode.car);
-		acg.setExcludedFuelTypes("gasoline,diesel");
+		acg.setExcludedFuelTypes("verbrenner");
 		config.addModule(acg);
 		
 		Scenario scenario = ScenarioUtils.createScenario(config);
@@ -212,7 +213,7 @@ public class AccessRestrictionExample {
 		
 		{
 			Person p = factory.createPerson(Id.createPersonId("p0"));
-			p.getAttributes().putAttribute("vehicleType", "gasoline");
+			p.getAttributes().putAttribute("vehicleType", "verbrenner");
 			
 			Plan plan = factory.createPlan();
 			Activity act1 = factory.createActivityFromCoord("dummy", new Coord(10,0));
